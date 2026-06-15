0df161949e9857c870b1b7ca34b026a263e11d22
Copstud3 <victorchris73@gmail.com>
2026-06-15 20:01:16 +0100

    Updating security rules.

--- src/main/java/com/chrisgmi/store/config/SecurityConfig.java
+++ src/main/java/com/chrisgmi/store/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(c -> c
                         .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/carts/**").permitAll()
                         .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
